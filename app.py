449adc05cc02fc7f83d8acffada9474de35841ae
Nicholas Pautler <nap44794@uga.edu>
2023-06-23 03:05:11 -0400

    Added basic starting bot code

---
+++ app.py
@@ -0,0 +1,37 @@
+# imports
+import discord
+from discord import app_commands
+import json
+
+# server IDs
+serverIDs = [371696021316829186]
+select = 0
+
+with open('info.json') as json_file:
+    info = json.load(json_file)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+intents.message_content = True
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@tree.command(name = "hello",\
+    description = "My first application Command",\
+    guild=discord.Object(serverIDs[select]))
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
+
+def run():
+    client.run(info['token'])
